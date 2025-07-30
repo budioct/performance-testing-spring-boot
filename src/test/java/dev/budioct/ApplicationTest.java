fbbc6a32c965a076d52563a9155a90a18dfd9c16
Budhi Octaviansyah <budioct@gmail.com>
2025-07-30 16:30:06 +0700

    project init

---
+++ src/test/java/dev/budioct/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dev.budioct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
